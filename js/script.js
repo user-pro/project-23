ccdaf1be82c2857e6648edf72fbbafafea397962
user-pro <65860162+user-pro@users.noreply.github.com>
2022-07-21 18:17:40 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,391 @@
+function initBurger(burgerSelector, menuSelector) {
+   const burger = document.querySelector(`${burgerSelector}`),
+      menu = document.querySelector(`${menuSelector}`),
+      body = document.querySelector('body');
+
+   burger.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      menu.classList.toggle('active');
+      body.classList.toggle('lock');
+   });
+};
+"use strict";
+
+function DynamicAdapt(type) {
+   this.type = type;
+}
+
+DynamicAdapt.prototype.init = function () {
+   const _this = this;
+   // массив объектов
+   this.оbjects = [];
+   this.daClassname = "_dynamic_adapt_";
+   // массив DOM-элементов
+   this.nodes = document.querySelectorAll("[data-da]");
+
+   // наполнение оbjects объктами
+   for (let i = 0; i < this.nodes.length; i++) {
+      const node = this.nodes[i];
+      const data = node.dataset.da.trim();
+      const dataArray = data.split(",");
+      const оbject = {};
+      оbject.element = node;
+      оbject.parent = node.parentNode;
+      оbject.destination = document.querySelector(dataArray[0].trim());
+      оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+      оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+      оbject.index = this.indexInParent(оbject.parent, оbject.element);
+      this.оbjects.push(оbject);
+   }
+
+   this.arraySort(this.оbjects);
+
+   // массив уникальных медиа-запросов
+   this.mediaQueries = Array.prototype.map.call(this.оbjects, function (item) {
+      return '(' + this.type + "-width: " + item.breakpoint + "px)," + item.breakpoint;
+   }, this);
+   this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
+      return Array.prototype.indexOf.call(self, item) === index;
+   });
+
+   // навешивание слушателя на медиа-запрос
+   // и вызов обработчика при первом запуске
+   for (let i = 0; i < this.mediaQueries.length; i++) {
+      const media = this.mediaQueries[i];
+      const mediaSplit = String.prototype.split.call(media, ',');
+      const matchMedia = window.matchMedia(mediaSplit[0]);
+      const mediaBreakpoint = mediaSplit[1];
+
+      // массив объектов с подходящим брейкпоинтом
+      const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
+         return item.breakpoint === mediaBreakpoint;
+      });
+      matchMedia.addListener(function () {
+         _this.mediaHandler(matchMedia, оbjectsFilter);
+      });
+      this.mediaHandler(matchMedia, оbjectsFilter);
+   }
+};
+
+DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
+   if (matchMedia.matches) {
+      for (let i = 0; i < оbjects.length; i++) {
+         const оbject = оbjects[i];
+         оbject.index = this.indexInParent(оbject.parent, оbject.element);
+         this.moveTo(оbject.place, оbject.element, оbject.destination);
+      }
+   } else {
+      for (let i = 0; i < оbjects.length; i++) {
+         const оbject = оbjects[i];
+         if (оbject.element.classList.contains(this.daClassname)) {
+            this.moveBack(оbject.parent, оbject.element, оbject.index);
+         }
+      }
+   }
+};
+
+// Функция перемещения
+DynamicAdapt.prototype.moveTo = function (place, element, destination) {
+   element.classList.add(this.daClassname);
+   if (place === 'last' || place >= destination.children.length) {
+      destination.insertAdjacentElement('beforeend', element);
+      return;
+   }
+   if (place === 'first') {
+      destination.insertAdjacentElement('afterbegin', element);
+      return;
+   }
+   destination.children[place].insertAdjacentElement('beforebegin', element);
+}
+
+// Функция возврата
+DynamicAdapt.prototype.moveBack = function (parent, element, index) {
+   element.classList.remove(this.daClassname);
+   if (parent.children[index] !== undefined) {
+      parent.children[index].insertAdjacentElement('beforebegin', element);
+   } else {
+      parent.insertAdjacentElement('beforeend', element);
+   }
+}
+
+// Функция получения индекса внутри родителя
+DynamicAdapt.prototype.indexInParent = function (parent, element) {
+   const array = Array.prototype.slice.call(parent.children);
+   return Array.prototype.indexOf.call(array, element);
+};
+
+// Функция сортировки массива по breakpoint и place 
+// по возрастанию для this.type = min
+// по убыванию для this.type = max
+DynamicAdapt.prototype.arraySort = function (arr) {
+   if (this.type === "min") {
+      Array.prototype.sort.call(arr, function (a, b) {
+         if (a.breakpoint === b.breakpoint) {
+            if (a.place === b.place) {
+               return 0;
+            }
+
+            if (a.place === "first" || b.place === "last") {
+               return -1;
+            }
+
+            if (a.place === "last" || b.place === "first") {
+               return 1;
+            }
+
+            return a.place - b.place;
+         }
+
+         return a.breakpoint - b.breakpoint;
+      });
+   } else {
+      Array.prototype.sort.call(arr, function (a, b) {
+         if (a.breakpoint === b.breakpoint) {
+            if (a.place === b.place) {
+               return 0;
+            }
+
+            if (a.place === "first" || b.place === "last") {
+               return 1;
+            }
+
+            if (a.place === "last" || b.place === "first") {
+               return -1;
+            }
+
+            return b.place - a.place;
+         }
+
+         return b.breakpoint - a.breakpoint;
+      });
+      return;
+   }
+};
+
+const da = new DynamicAdapt("max");
+da.init();;
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+};
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active-tab");
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).classList.add("active-tab");
+    evt.currentTarget.className += " active";
+
+
+}
+if(document.getElementById("defaultOpen")){
+
+    document.getElementById("defaultOpen").click();
+};
+function testWebP(callback) {
+
+   var webP = new Image();
+   webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+   };
+   webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+   if (support == true) {
+      document.querySelector('body').classList.add('webp');
+   } else {
+      document.querySelector('body').classList.add('no-webp');
+   }
+});;
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 400;
+
+if (popupLinks.length > 0) {
+   for (let index = 0; index < popupLinks.length; index++) {
+      const popupLink = popupLinks[index];
+      popupLink.addEventListener("click", function (e) {
+         const popupName = popupLink.getAttribute('href').replace('#', '');
+         const curentPopup = document.getElementById(popupName);
+         popupOpen(curentPopup);
+         e.preventDefault();
+      });
+   }
+}
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+   for (let index = 0; index < popupCloseIcon.length; index++) {
+      const el = popupCloseIcon[index];
+      el.addEventListener('click', function (e) {
+         popupClose(el.closest('.popup'));
+         e.preventDefault();
+      });
+   }
+}
+
+function popupOpen(curentPopup) {
+   if (curentPopup && unlock) {
+      const popupActive = document.querySelector('.popup.open');
+      if (popupActive) {
+         popupClose(popupActive, false);
+      } else {
+         bodyLock();
+      }
+      curentPopup.classList.add('open');
+      curentPopup.addEventListener("click", function (e){
+         if (!e.target.closest('.popup__content')) {
+            popupClose(e.target.closest('.popup'));
+         }
+      });
+   }
+}
+function popupClose(popupActive, doUnlock = true) {
+   if (unlock) {
+      popupActive.classList.remove('open');
+      if (doUnlock) {
+         bodyUnLock();
+      }
+   }
+}
+
+function bodyLock() {
+   const LockPaddingValue = window.innerWidth - document.querySelector('html').offsetWidth + 'px';
+   if (lockPadding.length > 0) {
+
+      for (let index = 0; index < lockPadding.length; index++) {
+         const el = lockPadding[index];   
+
+         el.style.paddingRight = LockPaddingValue;
+      }
+   }
+   body.style.paddingRight = LockPaddingValue;
+   body.classList.add('lock');
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
+}
+
+function bodyUnLock() {
+   setTimeout(function () {
+      for ( let index = 0; index < lockPadding.length; index++) {
+         const el = lockPadding[index];
+         el.style.paddingRight ='0px';
+      }
+      body.style.paddingRight = '0px';
+      body.classList.remove('lock');
+   }, timeout);
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout)
+}
+
+document.addEventListener('keydown', function (e) {
+   if (e.which === 27) {
+      const popupActive = document.querySelector('.popup.open');
+      popupClose(popupActive);
+   }
+});
+
+(function () {
+   if (!Element.prototype.closest) {
+      Element.prortotype.closest = function (css) {
+         var node = this;
+         while (node) {
+            if (node.matches(css)) return node;
+            else node = node.parentElement;
+         }
+         return null;
+      };
+   }
+})();
+(function () {
+   if (!Element.prototype.matches) {
+      Element.prototype.matches = Element.prototype.matchesMatchesSelector ||
+         Element.prototype.webkitMatchesSelector ||
+         Element.prototype.mozMatchesSelector ||
+         Element.prototype.sMatchesSelector;
+   }
+})();;
+const animItems = document.querySelectorAll('._anim-item');
+
+if (animItems.length > 0) {
+   window.addEventListener('scroll', animOnScroll);
+   function animOnScroll() {
+      for (let i = 0; i < animItems.length; i++) {
+         const animItem = animItems[i],
+            animItemHeight = animItem.offsetHeight,
+            animItemOffset = offset(animItem).top,
+            animStart = 4;
+
+         let animItemPoint = window.innerHeight - animItemHeight / animStart;
+         if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+         }
+
+         if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+            animItem.classList.add('_active');
+
+         } 
+      }
+   }
+   function offset(el) {
+      const rect = el.getBoundingClientRect(),
+         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+   }
+
+   setTimeout(() => {
+      animOnScroll();
+   }, 300);
+}
+;
+initBurger('.navbar__burger', '.navbar-menu_mobile');
+let header = document.querySelector('.header');
+if(window.pageYOffset > 1){
+   header.classList.add("active-header");
+}
+if(window.pageYOffset < 1){
+   header.classList.remove("active-header");
+}
+addEventListener('scroll', ()=>{
+   if(window.pageYOffset > 1){
+      header.classList.add("active-header");
+   }
+   if(window.pageYOffset < 1){
+      header.classList.remove("active-header");
+   }
+
+});
